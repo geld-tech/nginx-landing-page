d0fb01c75705def6a5517eba1fbdb191e3e1fce2
zlig <gdevillard@gmail.com>
2018-05-27 20:01:47 +0100

    Modify Flask service to do something and add logo in navbar

--- sources/opt/geld/webapps/landing-page/application.py
+++ sources/opt/geld/webapps/landing-page/application.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 app.debug=True
 
 @app.route("/")
-@app.route("/hello/<name>")
-def index(name=None):
-    return render_template('index.html', name=name)
+@app.route("/<section>")
+def index(section=None):
+	page_title="geld.tech"
+	return render_template('index.html', title=page_title, section=section)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
 
