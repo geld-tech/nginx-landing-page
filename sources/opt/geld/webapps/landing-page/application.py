b098d7228a7289d3c68ff4762723462a614c5fb1
zlig <gdevillard@gmail.com>
2018-05-28 11:49:49 +0100

    Adds library and functions to parse nginx config files

--- sources/opt/geld/webapps/landing-page/application.py
+++ sources/opt/geld/webapps/landing-page/application.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python
+import glob
 from flask import Flask, render_template
+from dependencies.nginxparser import load
 
+# Globals
 app = Flask(__name__)
 app.debug=True
+
+NGINX_CONFD_FILES = "/etc/nginx/conf.d/*.conf"
+keypairs = {}
+
+
+# Functions
+
+def parse_config(input, data):
+	if isinstance(input, list):
+		if len(input)  == 2 and not isinstance(input[0], list):
+			data[input[0]] = input[1]
+	else:
+		for item in input:
+		parse_config(item, data)
+
+
+def parse_nginx_config_files(directory):
+	locations = []
+	nginx_config_files = glob.glob(directory)
+	for config_file in nginx_config_files:
+		nginx_config_data = {}
+		nginx_config_contents = nginxparser.load(open(config_file))
+		parse_config(nginx_config_contents, nginx_config_data)
+		if nginx_config_data.has_key('location'):
+			locations.append(nginx_config_data.get('location'))
+	return locations
+
+
+# Flask App and Routes
 
 @app.route("/")
 @app.route("/<section>")
@@ -15,5 +47,7 @@ def page_not_found(e):
 	return render_template('404.html'), 404
 
 if __name__ == "__main__":
+	locations = parse_nginx_config_files(NGINX_CONFD_FILES)
+		
 	app.run(host='0.0.0.0')
 
