a8aae9650a98bdf30264913d1ce94c1507544824
zlig <gdevillard@gmail.com>
2018-07-24 20:56:12 +0100

    Explain how to downgrade on a better way

--- sources/opt/geld/webapps/nginx-landing-page/dependencies/screenshotgenerator.py
+++ sources/opt/geld/webapps/nginx-landing-page/dependencies/screenshotgenerator.py
@@ -20,7 +20,8 @@ def take_screenshot(url, filename="screenshot.png", width="1024", height="768", 
         chrome_options.add_argument("--hide-scrollbars")
         chrome_options.add_argument("window-size=%s,%s" % (width, height))
 
-        # Chromium and/or WebDriver can become broken during updates or distribution upgrades, for the following packages:
+        # Chromium and/or WebDriver can become broken during updates or distribution upgrades.
+        # One of the possible solution to fix these issues was do downgrade the following packages (download .deb files and install locally with `sudo apt install ./chromium-*`):
         #   - chromium-browser
         #   - chromium-browser-l10n
         #   - chromium-chromedriver
